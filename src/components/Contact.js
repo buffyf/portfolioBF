e7929f27d0417f557fed71e7407328478f5c614c
Buffy Feinstein <buffyfeinstein@gmail.com>
2017-10-05 16:23:43 -0500

    fixed pdf

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "../styles/contact.css";
-// import resume from '../BF_Resume.pdf';
+import pdf from "../assets/BF_Resume.pdf"
 
 export default class Contact extends Component {
 
@@ -12,16 +12,13 @@ export default class Contact extends Component {
                         <div className="col-md-8" id="contact-wrapper">
                             <h2 id="contact-title">Contact</h2>
                             <div id="contact-info">
-
                                 <div id="icons">
                                     <a href="https://github.com/buffyf" target="_blank"><i className="fa fa-github-square social-icons" id="github" aria-hidden="true"></i>See My Code</a>
                                     <a href="https://www.linkedin.com/in/buffyfeinstein/" target="_blank"><i className="fa fa-linkedin-square social-icons" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;Follow Me</a>
                                     <a href=""><i className="fa fa-phone-square social-icons" aria-hidden="true"></i>847.942.7691</a>
                                     <a href="mailto:buffyfeinstein@gmail.com" target="_blank"><i className="fa fa-envelope-square social-icons" aria-hidden="true"></i>Message Me</a>
-                                    <a href="../BF_Resume.pdf" target="_blank"><i className="fa fa-arrow-circle-down social-icons" aria-hidden="true"></i>View My Resume</a>
-
+                                    <a href={pdf} target="blank"><i className="fa fa-arrow-circle-down social-icons" aria-hidden="true"></i>View My Resume</a>
                                 </div>
-
                             </div>
                         </div>
                     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,30 +13,32 @@ export default class Skills extends Component {
                     <h2 id="skills-title">Skills</h2>
                     <div id="table">
                         <table>
-                            <tr>
-                                <td><div className='cell-bulk style1'></div><div className='cell-text'>JAVASCRIPT</div></td>
-                                <td><div className='cell-bulk style2'></div><div className='cell-text'>REACT</div></td>
-                                <td><div className='cell-bulk style3'></div><div className='cell-text'>EXPRESS</div></td>
-                                <td><div className='cell-bulk style4'></div><div className='cell-text'>SEQUELIZE</div></td>
-                                <td><div className='cell-bulk style5'></div><div className='cell-text'>SQL</div></td>
-                                <td><div className='cell-bulk style6'></div><div className='cell-text'>VS CODE</div></td>
-                            </tr>
-                            <tr>
-                                <td><div className='cell-bulk style7'></div><div className='cell-text'>HTML</div></td>
-                                <td><div className='cell-bulk style8'></div><div className='cell-text'>BOOTSTRAP</div></td>
-                                <td><div className='cell-bulk style9'></div><div className='cell-text'>NODE.JS</div></td>
-                                <td><div className='cell-bulk style10'></div><div className='cell-text'>POSTGRESQL</div></td>
-                                <td><div className='cell-bulk style11'></div><div className='cell-text'>HANDLEBARS</div></td>
-                                <td><div className='cell-bulk style12'></div><div className='cell-text'>SLACK</div></td>
-                            </tr>
-                            <tr>
-                                <td><div className='cell-bulk style13'></div><div className='cell-text'>CSS</div></td>
-                                <td><div className='cell-bulk style14'></div><div className='cell-text'>ADOBE CC</div></td>
-                                <td><div className='cell-bulk style15'></div><div className='cell-text'>MONGO DB</div></td>
-                                <td><div className='cell-bulk style16'></div><div className='cell-text'>TRELLO</div></td>
-                                <td><div className='cell-bulk style17'></div><div className='cell-text'>GIT/ GITHUB</div></td>
-                                <td><div className='cell-bulk style18'></div><div className='cell-text'>AGILE</div></td>
-                            </tr>
+                            <tbody>
+                                <tr>
+                                    <td><div className='cell-bulk style1'></div><div className='cell-text'>JAVASCRIPT</div></td>
+                                    <td><div className='cell-bulk style2'></div><div className='cell-text'>REACT</div></td>
+                                    <td><div className='cell-bulk style3'></div><div className='cell-text'>EXPRESS</div></td>
+                                    <td><div className='cell-bulk style4'></div><div className='cell-text'>SEQUELIZE</div></td>
+                                    <td><div className='cell-bulk style5'></div><div className='cell-text'>SQL</div></td>
+                                    <td><div className='cell-bulk style6'></div><div className='cell-text'>VS CODE</div></td>
+                                </tr>
+                                <tr>
+                                    <td><div className='cell-bulk style7'></div><div className='cell-text'>HTML</div></td>
+                                    <td><div className='cell-bulk style8'></div><div className='cell-text'>BOOTSTRAP</div></td>
+                                    <td><div className='cell-bulk style9'></div><div className='cell-text'>NODE.JS</div></td>
+                                    <td><div className='cell-bulk style10'></div><div className='cell-text'>POSTGRESQL</div></td>
+                                    <td><div className='cell-bulk style11'></div><div className='cell-text'>HANDLEBARS</div></td>
+                                    <td><div className='cell-bulk style12'></div><div className='cell-text'>SLACK</div></td>
+                                </tr>
+                                <tr>
+                                    <td><div className='cell-bulk style13'></div><div className='cell-text'>CSS</div></td>
+                                    <td><div className='cell-bulk style14'></div><div className='cell-text'>ADOBE CC</div></td>
+                                    <td><div className='cell-bulk style15'></div><div className='cell-text'>MONGO DB</div></td>
+                                    <td><div className='cell-bulk style16'></div><div className='cell-text'>TRELLO</div></td>
+                                    <td><div className='cell-bulk style17'></div><div className='cell-text'>GIT/ GITHUB</div></td>
+                                    <td><div className='cell-bulk style18'></div><div className='cell-text'>AGILE</div></td>
+                                </tr>
+                            </tbody>
                         </table>
                     </div>
                 </div>
