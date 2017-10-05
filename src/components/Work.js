fca15c54aa67ce218ce75db420ba111c92e96c47
Buffy Feinstein <buffyfeinstein@gmail.com>
2017-10-05 15:05:06 -0500

    added resume pdf

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,7 +13,7 @@ export default class Work extends Component {
                     <div className="row project">
                         <video className="d-block img-fluid col-md-8 project-img" src={bakery} controls controlsList="nodownload"></video>                        <div className="col-md-4" id="project-info">
                             <h5>Billy's Bakery: E-Commerce Site</h5>
-                            <p>This is a mock e-commerce bakery site built with React. The overall goal of this project was to utilize the React framework along with React Routing to dynamically generate URL endpoints for a detail element page on a parent element page using React Router 4. </p>
+                            <p>This is a mock e-commerce bakery site built with React. The overall goal of this project was to utilize the React framework along with React routing to dynamically generate URL endpoints for a detail element page on a parent element page using React Router 4. </p>
                             <p>Technologies: React, React-Router, Bootstrap, CSS, and HTML</p>
                             <a href="https://billys-bakery.surge.sh" className="live-btn" target="_blank">Live Site</a>
                             <a href="https://github.com/buffyf/eCommerceSite" className="github-btn" target="_blank">GitHub</a>
@@ -32,7 +32,7 @@ export default class Work extends Component {
                     <div className="row project">
                         <video className="d-block img-fluid col-md-8 project-img" src={gabble} controls controlsList="nodownload"></video>                        <div className="col-md-4" id="project-info">
                             <h5>Gabble: Social Site</h5>
-                            <p>Gabble is a social site app that allows users to signup for an account, login, create and delete posts, like and unlike posts, and also view a list of a comment's likes.</p>
+                            <p>Gabble is a social site that allows users to signup for an account, login, create and delete posts, like and unlike posts, and also view a list of a comment's likes.</p>
                             <p>Technologies: Node.js, Express, Sequelize, PostgreSQL, Mustache, and Amazon Web Services</p>
                             <a href="http://buffy-gabble.xyz" className="live-btn" target="_blank">Live Site</a>
                             <a href="https://github.com/buffyf/project_gabble" className="github-btn" target="_blank">GitHub</a>
