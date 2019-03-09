36b782018ac8666e259a4641ebd13cebb54f4bc1
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-03-09 17:39:50 -0600

    update description

---
+++ src/components/Home.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+// import { NavLink } from "react-router-dom"
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="row home">
+                <div className="wrap">
+                    <div className="block style-1 h1"></div>
+                    <div className="block style-2 h2"></div>
+                    <div className="block style-3 h3"></div>
+                    <div className="block style-4 h4"></div>
+                    <div className="block style-5 h5"></div>
+                    <div className="block style-6 h6"></div>
+                    <div className="block style-7 h7"></div>
+                    <div className="block style-8 h8"></div>
+                    <div className="block style-9 h9"></div>
+                    <div className="block-title">
+                        <div className="title-1">
+                            <span>B</span>
+                            <span>U</span>
+                            <span>F</span>
+                            <span>F</span>
+                            <span>Y</span>
+                        </div>
+                        <div className="title-3">
+                            <span>F</span>
+                            <span>E</span>
+                            <span>I</span>
+                            <span>N</span>
+                            <span>S</span>
+                            <span>T</span>
+                            <span>E</span>
+                            <span>I</span>
+                            <span>N</span>
+                        </div>
+                        <div className="title-2">
+                            <span>W</span>
+                            <span>E</span>
+                            <span>B</span>
+                            <span>&nbsp;</span>
+                            <span>&</span>
+                            <span>&nbsp;</span>
+                            <span>M</span>
+                            <span>O</span>
+                            <span>B</span>
+                            <span>I</span>
+                            <span>L</span>
+                            <span>E</span>
+                            <span>&nbsp;</span>
+                            <span>D</span>
+                            <span>E</span>
+                            <span>V</span>
+                            <span>E</span>
+                            <span>L</span>
+                            <span>O</span>
+                            <span>P</span>
+                            <span>E</span>
+                            <span>R</span>
+                        </div>
+                    </div>
+                    <div className="block style-10 h10"></div>
+                    <div className="block style-6 h11"></div>
+                    <div className="block style-3 h12"></div>
+                    <div className="block style-12 h13"></div>
+                    <div className="block style-1 h14"></div>
+                    <div className="block style-11 h15"></div>
+                    <div className="block style-7 h16"></div>
+                    <div className="block style-2 h17"></div>
+                    <div className="block-13 h18"></div>
+                </div>
+                <div>
+                    <div id="bio" className="col-md-9 bio">
+                        <div id="about-container">
+                            <div className="about-box">
+                                <div className="text">
+                                    <p id="aboutText">Hi, I'm Buffy. I'm a web and iOS mobile developer and designer based out of Chicago, Illinois. Before pivoting my career to development, I was part of many different collaborative creative processes. Whether coordinating project logistics, integrating technology in the classroom, or baking and decorating the perfect cake - I love making awesome things.
+                                        <br />
+                                        <br />
+                                        When I'm not coding, you'll find me baking, making, hiking, biking, cooking, booking, dining, and reclining.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
